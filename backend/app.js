25412511e9beac7e656c36a6954da9624ce1e7d6
Konstantin Vasilyev <hello.vasilyev@gmail.com>
2024-02-03 14:10:17 +0300

    пр15 фикс 2

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { login, createUser } = require('./controllers/users');
 const cardRouter = require('./routes/cards');
 
-const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
@@ -44,9 +44,11 @@ app.use('/cards', auth, cardRouter);
 
 app.use(errorLogger);
 
+//
 // app.use((req, res, next) => {
 //   next(new NotFoundError('Страница не найдена'));
 // });
+//
 
 app.use(errors());
 
